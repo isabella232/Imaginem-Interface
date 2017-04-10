12f73be0abce722f3759b6ffae7ad2f743d0a2fb
Jason Fox <valknut13@gmail.com>
2017-04-10 10:28:13 -0500

    fix up home page content

--- src/components/LandingContent.js
+++ src/components/LandingContent.js
@@ -7,14 +7,14 @@ export default class LandingContent extends Component {
         return (
             <div>
                 <div>
-                    <h1>Welcome to Imàginem (Latin for image)</h1>
-                    <h3>Imàginem is an open source, serverless framework for processing images through a number of computer vision classifiers, including face recognition and matching.</h3>
+                    <h1>Welcome to Imaginem (Latin for image)</h1>
+                    <h3>Imaginem is an open source, serverless framework for processing images through a number of computer vision classifiers, including face recognition and matching.</h3>
                 </div>
                 <br/>
                 <br/>
                 <div className="content">
                     <p>
-                        When we set out to build Imàginem, we wanted it to be easy to configure, deploy and customize. That is why Imàginem is built on the <a href="https://azure.microsoft.com/en-us/services/functions/">Azure Functions</a> serverless framework. 
+                        When we set out to build Imaginem, we wanted it to be easy to configure, deploy and customize. That is why Imaginem is built on the <a href="https://azure.microsoft.com/en-us/services/functions/">Azure Functions</a> serverless framework. 
                         The serverless architecture allows developers to quickly add new steps to the pipeline by simply writing a new Function and adding it your Function App. 
                         <br/>
                         <br/>
@@ -22,7 +22,7 @@ export default class LandingContent extends Component {
                         becuase they are easily accessible, easy to use and provide a powerful representation of the types of image classificatiosn that we're trying to demonstrate. Since the frmaeowkr is open source, feel free to use any service or executable that fits your needs.
                     </p>
                     <p>
-                        If you're interested in using Imàginem, please feel free to check out the <a href="/#/upload">demo</a> and clone the GitHub <a href="https://github.com/CatalystCode/Imaginem">repository</a>.
+                        If you're interested in using Imaginem, please feel free to check out the <a href="/#/upload">demo</a> and clone the GitHub <a href="https://github.com/CatalystCode/Imaginem">repository</a>.
                     </p>
                 </div>
             </div>
